4f7fc07ca74ee21342ebbfdaa403c130b21c5db0
Thomas Arildsen <tha@es.aau.dk>
2016-10-13 13:59:03 +0200

    Corrected minor but essential omission in 'run_main.m'

--- run_main.m
+++ run_main.m
@@ -3,7 +3,7 @@
 % `true`.
 
 cd reprod-vila2011/
-%phasetrans_old_unif
+phasetrans_old_unif
 copyfile vila2011_results_old_unif.fig ../vila2011_main_results.fig
 copyfile vila2011_results_old_unif.mat ../vila2011_main_results.mat
 copyfile vila2011_results_old_unif.pdf ../vila2011_main_results.pdf
